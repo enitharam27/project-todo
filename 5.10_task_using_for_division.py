9165ff870b6f49a8c38f84e70b1056324c31f811
enitharam27 <153385458+enitharam27@users.noreply.github.com>
2023-12-30 11:09:31 +0530

    Add files via upload

---
+++ 5.10_task_using_for_division.py
@@ -0,0 +1,20 @@
+print('5th division table')
+print('-------------------')
+for i in range(1,11):
+    print(i,'/5=',i/5)
+    print('')
+print('7th division table')
+print('--------------------')
+for i in range(1,11):
+    print(i,'/7=',i/7)
+    print('')
+print('9th division table')
+print('------------------')
+for i in range(1,11):
+    print(i,'/9=',i/9)
+    print('')
+print('11th division table')
+print('--------------------')
+for i in range(1,11):
+    print(i,'/11=',i/11)
+    
